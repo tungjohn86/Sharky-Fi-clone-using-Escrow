b9f7e0f102b72b559609c1f4b54ed15c8a31120d
tungjohn86 <158741088+tungjohn86@users.noreply.github.com>
2024-02-03 19:36:41 +0700

    Update lib.rs: update program id declare

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("DJ2yQBUMwFkB3cWsVbUZZ4DxmkR4pFf6KxrZMEDD1uwH");
 
 #[program]
 pub mod nft_lend_borrow {
